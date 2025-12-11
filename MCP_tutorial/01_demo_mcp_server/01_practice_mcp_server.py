b81753a2ff115be7a3563472606f979d4a07a7ed
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-12-11 11:12:07 +0500

    MCP server, make a demo server with two tools

---
+++ MCP_tutorial/01_demo_mcp_server/01_practice_mcp_server.py
@@ -0,0 +1,22 @@
+import random
+from fastmcp import FastMCP
+
+# Create MCP server instance
+mcp = FastMCP(name="Demo Server")
+
+@mcp.tool
+def roll_dice(n_dice: int = 1) -> list[int]:
+    """Roll n_dice 6-sided dice and return the results."""
+    return [random.randint(1, 6) for _ in range(n_dice)]
+    
+@mcp.tool
+def add_numbers(a: float, b: float) -> int:
+    """Add n numbers."""
+    return a + b
+
+if __name__ == "__main__":
+    # for testing the server: uv run fastmcp dev 01_practice_mcp_server.py
+    # for tunning the server: uv run fastmcp run 01_practice_mcp_server.py
+    # for inegrating it to the claude desktop run: uv run fastmcp install claude-desktop 01_practice_mcp_server.py
+    mcp.run()
+    
